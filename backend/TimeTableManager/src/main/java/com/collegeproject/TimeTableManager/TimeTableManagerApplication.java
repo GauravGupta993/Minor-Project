775724325613b0bc33d5698c3f39d79c39275254
GauravGupta993 <gaurav115993@gmail.com>
2025-02-20 23:04:23 +0530

    Basic Structure with Login Page.

---
+++ backend/TimeTableManager/src/main/java/com/collegeproject/TimeTableManager/TimeTableManagerApplication.java
@@ -0,0 +1,13 @@
+package com.collegeproject.TimeTableManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTableManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeTableManagerApplication.class, args);
+	}
+
+}
---
+++ backend/TimeTableManager/src/test/java/com/collegeproject/TimeTableManager/TimeTableManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collegeproject.TimeTableManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTableManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

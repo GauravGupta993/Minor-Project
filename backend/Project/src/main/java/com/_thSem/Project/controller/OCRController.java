425502e16116d6f12b0d8002662ad17e5352a9d4
Anubhavpandey27 <anubhavpandey.bt22cseds@pec.edu.in>
2025-03-18 22:44:07 +0530

    timetableapi

--- backend/Project/src/main/java/com/_thSem/Project/controller/OCRController.java
+++ backend/Project/src/main/java/com/_thSem/Project/controller/OCRController.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import net.sourceforge.tess4j.ITesseract;
-import net.sourceforge.tess4j.Tesseract;
-import net.sourceforge.tess4j.TesseractException;
+//import net.sourceforge.tess4j.ITesseract;
+//import net.sourceforge.tess4j.Tesseract;
+//import net.sourceforge.tess4j.TesseractException;
 
 import java.io.File;
 
@@ -14,19 +14,19 @@ import java.io.File;
 @RequestMapping("/api")
 public class OCRController {
 
-    private final String UPLOAD_DIR = "uploads/";
-
-    @GetMapping("/ocr")
-    public String performOCR(@RequestParam("filename") String filename) {
-        File imageFile = new File(UPLOAD_DIR + filename);
-
-        ITesseract instance = new Tesseract();
-        instance.setDatapath("tessdata"); // Path to Tesseract trained data
-
-        try {
-            return instance.doOCR(imageFile);
-        } catch (TesseractException e) {
-            return "Error processing image: " + e.getMessage();
-        }
-    }
+//    private final String UPLOAD_DIR = "uploads/";
+//
+//    @GetMapping("/ocr")
+//    public String performOCR(@RequestParam("filename") String filename) {
+//        File imageFile = new File(UPLOAD_DIR + filename);
+//
+//        ITesseract instance = new Tesseract();
+//        instance.setDatapath("tessdata"); // Path to Tesseract trained data
+//
+//        try {
+//            return instance.doOCR(imageFile);
+//        } catch (TesseractException e) {
+//            return "Error processing image: " + e.getMessage();
+//        }
+//    }
 }
